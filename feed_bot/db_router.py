f6bff74dd3c0dea80d974450814c56a991f791d3
LeonXBB <lbarenbojm@gmail.com>
2022-04-21 22:13:24 +0300

    initial commit

--- feed_bot/db_router.py
+++ feed_bot/db_router.py
@@ -1,7 +1,3 @@
 routes = {
     "postgres": ("Action", "CoinToss", "Competition", "Event", "EventTemplate", "EventTemplateEdit", "Period", "Point", "RulesSet", "SideChange", "Team", "TimeOut", "Timer", "TextString", "TextLanguage", "PasswordPair", "BotUser", "ScheduledMessage", "APIMessage"),
-    #"logic": ("Action", "CoinToss", "Competition", "Event", "EventTemplate", "EventTemplateEdit", "Period", "Point", "RulesSet", "SideChange", "Team", "TimeOut", "Timer"),
-    #"localization": ("TextString", "TextLanguage"),
-    #"telebot": ("PasswordPair", "BotUser", "ScheduledMessage"),
-    #"website": ("APIMessage",)
 }
