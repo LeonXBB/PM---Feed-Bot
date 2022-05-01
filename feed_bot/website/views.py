84da7b4b9d8592460ce3392769b9edfbe6fcbee4
LeonXBB <lbarenbojm@gmail.com>
2022-05-02 02:37:32 +0300

    balancer check

--- feed_bot/website/views.py
+++ feed_bot/website/views.py
@@ -6,11 +6,17 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+import time
+
 @method_decorator(csrf_exempt, name="dispatch")
 class Index(TemplateView):
 
     def get(self, request):
-        return render(request, "index/index.html", {"server_address": config("server_address")}, status=200)
-    
+        print('\n', time.strftime("%H:%M:%S"), 'received get request, trying to reply...')
+        try:
+            return render(request, "index/index.html", {"server_address": config("server_address")}, status=200)
+        except Exception as e:
+            print('\n', time.strftime("%H:%M:%S"), 'error occured:', e)
+
     def post(self, request):
         pass
