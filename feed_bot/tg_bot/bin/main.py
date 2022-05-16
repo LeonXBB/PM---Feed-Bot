1e20461b891d6a3a279eacfbe4917e8edf223c3d
LeonXBB <lbarenbojm@gmail.com>
2022-05-16 17:07:48 +0300

    fix for wrong indexing in sending empty remainders

--- feed_bot/tg_bot/bin/main.py
+++ feed_bot/tg_bot/bin/main.py
@@ -72,7 +72,7 @@ class FeedBot(TeleBot):
                     for button in row:
                         
                         button_formatter = data[3][messages_count + len(keyboards)][i] if data[3] is not None and len(data[3]) > messages_count + len(keyboards) and data[3][messages_count + len(keyboards)] is not None and len(data[3][messages_count + len(keyboards)]) > i else list()
-                        callback_formatter = data[4][len(keyboards) - 1][i] if data[4] is not None and len(data[4]) >= len(keyboards) and data[4][len(keyboards)] is not None and len(data[4][len(keyboards)]) > i else list()
+                        callback_formatter = data[4][len(keyboards) - 1][i] if data[4] is not None and len(data[4]) >= len(keyboards) and data[4][len(keyboards) - 1] is not None and len(data[4][len(keyboards) - 1]) > i else list()
                         
                         keyboard[-1].append(InlineKeyboardButton(button["text"][user_language_id].format(button_formatter), callback_data=button["data"].format(callback_formatter)))
                         i += 1
